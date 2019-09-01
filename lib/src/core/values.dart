294cc843abc6f67f7464cf2a4129b4288a5438f2
predatorx7 <smushaheed@outlook.com>
2019-09-01 21:39:01 +0530

    Added ImageIcons, place holders, logos & gservice

---
+++ lib/src/core/values.dart
@@ -0,0 +1,10 @@
+// Colors
+int colorPrimary = 0xff455cde,
+    colorPrimaryDark = 0xff403d3d,
+    colorAccent = 0xffc2bdbf,
+    colorBlack = 0xff000000,
+    colorGreen = 0xff4cab03;
+// Strings
+String appName = 'InstagramTest',
+    /* TODO: Remove or change this placeholder text */
+    helloBlankFragment = 'Hello blank fragment';
